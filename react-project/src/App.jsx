6e8bb9eca736a70bab55916ad71f5770a4cf90b6
Debanjan <debanjan.py@gmail.com>
2024-10-05 22:10:29 +0530

    n

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import AxiosCall from './Core/Api/AxiosCall'
-import ReactUseQuery from './Core/Cache/UseQuery'
 
 function App() {
   return (
     <div className='p-1 m-1'>
- 
+
     </div>
   )
 }
